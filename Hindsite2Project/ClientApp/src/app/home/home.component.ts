140d0c2020b9815e308f549b6d4666902e32fa99
breen <jon16017@byui.edu>
2019-07-13 19:49:36 -0600

    Commit changes, before started deleted.

--- Hindsite2Project/ClientApp/src/app/home/home.component.ts
+++ Hindsite2Project/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
 }
