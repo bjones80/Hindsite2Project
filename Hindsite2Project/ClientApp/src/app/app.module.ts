0854461ff6afe9397672c70c144e8834a4cd47e3
breen <jon16017@byui.edu>
2019-07-13 20:06:49 -0600

    Deleted stuff and got it back to displaying stuff, but it doesn't display right

--- Hindsite2Project/ClientApp/src/app/app.module.ts
+++ Hindsite2Project/ClientApp/src/app/app.module.ts
@@ -1,34 +1,74 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
-
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ClientsComponent } from './clients/clients.component';
+import { YepComponent } from './yep/yep.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeGalleryComponent } from './home/home-gallery/home-gallery.component';
+import { HomeLandComponent } from './home/home-land/home-land.component';
+import { HomeWeedControlComponent } from './home/home-weed-control/home-weed-control.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AboutHindsiteComponent } from './about/about-hindsite/about-hindsite.component';
+import { AboutGalleryComponent } from './about/about-gallery/about-gallery.component';
+import { AboutBossComponent } from './about/about-boss/about-boss.component';
+import { AboutTimeLineComponent } from './about/about-time-line/about-time-line.component';
+import { AboutBossGalleryComponent } from './about/about-boss-gallery/about-boss-gallery.component';
+import { FooterComponent } from './footer/footer.component';
+import { ReclamationComponent } from './services/reclamation/reclamation.component';
+import { WeedControlComponent } from './services/weed-control/weed-control.component';
+import { MonitoringComponent } from './services/monitoring/monitoring.component';
+import { ContactDetailComponent } from './contact/contact-detail/contact-detail.component';
+import { ContactMessageComponent } from './contact/contact-message/contact-message.component';
+import { ContactLocatedComponent } from './contact/contact-located/contact-located.component';
+import { HomeQuoteComponent } from './home/home-quote/home-quote.component';
+import { PaytimeComponent } from './yep/paytime/paytime.component';
+import { CalendarComponent } from './yep/calendar/calendar.component';
+import { YepDefinitionComponent } from './yep/yep-definition/yep-definition.component';
+import { ClientsTableComponent } from './clients/clients-table/clients-table.component';
+import { ClientsLoginComponent } from './clients/clients-login/clients-login.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
+    HeaderComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    AboutComponent,
+    ServicesComponent,
+    ClientsComponent,
+    YepComponent,
+    ContactComponent,
+    HomeGalleryComponent,
+    HomeLandComponent,
+    HomeWeedControlComponent,
+    AboutHindsiteComponent,
+    AboutGalleryComponent,
+    AboutBossComponent,
+    AboutTimeLineComponent,
+    AboutBossGalleryComponent,
+    FooterComponent,
+    ReclamationComponent,
+    WeedControlComponent,
+    MonitoringComponent,
+    ContactDetailComponent,
+    ContactMessageComponent,
+    ContactLocatedComponent,
+    HomeQuoteComponent,
+    PaytimeComponent,
+    CalendarComponent,
+    YepDefinitionComponent,
+    ClientsTableComponent,
+    ClientsLoginComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
